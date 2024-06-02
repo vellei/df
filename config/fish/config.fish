2ab90389b6e1065a53fe5ea4446240ddd948d27c
larmryan <larmryan3@gmail.com>
2024-06-01 23:12:07 -0500

    fix: ssh-agent started once per terminal session; mpd not started

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,16 +2,23 @@ set fish_greeting
 
 if set -q ZELLIJ
 else
-  zellij
+    zellij
+end
+
+if ! pgrep mpd >/dev/null
+    mpd ~/.config/mpd/mpd.conf
+end
+
+if ! pgrep ssh-agent >/dev/null
+    eval (ssg-agent -c)
 end
 
 fish_config theme choose catppuccin_mocha
 
 set -x ZELLIJ_AUTO_EXIT 1
-set -x MANROFFOPT "-c" 
+set -x MANROFFOPT -c
 set -x MANPAGER "sh -c 'col -bx | /usr/bin/bat -l man -p'"
-set -x BAT_THEME "catppuccin_mocha"
-
+set -x BAT_THEME catppuccin_mocha
 
 set -x VISUAL hx
 set -x PAGER bat
